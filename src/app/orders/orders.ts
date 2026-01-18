4f6df6099be813180d671581b164150f011a1b60
Wandreyverson <wandreyverson.js@gmail.com>
2026-01-18 01:55:16 -0300

    fix: add and update

--- src/app/orders/orders.ts
+++ src/app/orders/orders.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule, NgForOf, NgIf, CurrencyPipe, DatePipe } from '@angular/common';
 import { Orders } from '../api/services/orders/orders.model';
 import { OrdersService } from '../api/services/orders/orders.service';
@@ -11,29 +11,34 @@ import { NewOrderModalComponent } from './new-orders-modal/new-orders-modal';
   standalone: true,
   imports: [CommonModule, NgForOf, NgIf, CurrencyPipe, DatePipe, NewOrderModalComponent]
 })
-
 export class OrdersComponent implements OnInit {
   orders: Orders[] = [];
   loading = true;
   error = '';
   showNewOrderModal = false;
 
-  constructor(private ordersService: OrdersService) { }
+  constructor(
+    private ordersService: OrdersService,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
     this.loadOrders();
   }
 
   loadOrders() {
+    this.loading = true;
     this.ordersService.getOrders().subscribe({
       next: (data) => {
-        this.orders = data;
+        this.orders = [...data];
         this.loading = false;
+        this.cdr.detectChanges();
       },
       error: (err) => {
         console.error(err);
         this.error = 'Erro ao carregar pedidos';
         this.loading = false;
+        this.cdr.detectChanges();
       }
     });
   }
@@ -47,16 +52,17 @@ export class OrdersComponent implements OnInit {
   }
 
   addNewOrder(order: Orders) {
-    this.orders = [order, ...this.orders];
+    this.showNewOrderModal = false;
+    this.loadOrders();
   }
 
   updateOrderStatus(order: Orders, status: Orders['status']) {
     this.ordersService.updateStatus(order.id, status).subscribe({
       next: (updatedOrder) => {
-        const index = this.orders.findIndex(o => o.id === updatedOrder.id);
-        if (index > -1) {
-          this.orders[index].status = updatedOrder.status;
-        }
+        this.orders = this.orders.map(o =>
+          o.id === updatedOrder.id ? { ...o, status: updatedOrder.status } : o
+        );
+        this.cdr.detectChanges();
       },
       error: (err) => {
         console.error('Erro ao atualizar status:', err);
@@ -64,4 +70,3 @@ export class OrdersComponent implements OnInit {
     });
   }
 }
-
